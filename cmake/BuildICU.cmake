60c76500cb6a6b484e2a2a92581274c510a02f58
Alexandros Roussos <thealexroussos@gmail.com>
2018-05-07 00:54:00 +0200

    Restructured, added prebuilts

---
+++ cmake/BuildICU.cmake
@@ -0,0 +1,149 @@
+# MIT License
+#
+# Copyright (c) 2018 The ViaDuck Project
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# build icu locally
+
+# includes
+include(ProcessorCount)
+include(ExternalProject)
+
+# find programs
+find_program(MAKE_PROGRAM make)
+
+# used to apply patches to ICU
+find_program(PATCH_PROGRAM patch)
+if (NOT PATCH_PROGRAM)
+    message(FATAL_ERROR "Cannot find patch utility.")
+endif()
+
+# set variables
+ProcessorCount(NUM_JOBS)
+
+# try to compile icu
+string(REPLACE "." "_" ICU_URL_VERSION ${ICU_BUILD_VERSION})
+set(ICU_URL http://download.icu-project.org/files/icu4c/${ICU_BUILD_VERSION}/icu4c-${ICU_URL_VERSION}-src.tgz)
+
+# download and unpack if needed
+if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/icu)
+    file(DOWNLOAD ${ICU_URL} ${CMAKE_CURRENT_BINARY_DIR}/icu_src.tgz SHOW_PROGRESS)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E tar x icu_src.tgz)
+endif()
+
+# if we are actually building for host, use cmake params for it
+if (NOT ICU_CROSS_ARCH)
+    set(HOST_CFLAGS "${CMAKE_C_FLAGS}")
+    set(HOST_CXXFLAGS "${CMAKE_CXX_FLAGS}")
+    set(HOST_CC "${CMAKE_C_COMPILER}")
+    set(HOST_CXX "${CMAKE_CXX_COMPILER}")
+    set(HOST_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
+    
+    set(HOST_ENV_CMAKE ${CMAKE_COMMAND} -E env
+            CC=${HOST_CC}
+            CXX=${HOST_CXX}
+            CFLAGS=${HOST_CFLAGS}
+            CXXFLAGS=${HOST_CXXFLAGS}
+            LDFLAGS=${HOST_LDFLAGS}
+    )
+    
+    # force CMake-reload
+    set(HOST_CMAKE_RELOAD ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} ${CMAKE_BINARY_DIR})
+else()
+    set(HOST_CMAKE_RELOAD true)
+endif()
+
+ExternalProject_Add(
+        icu_host
+        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu
+        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_host-build
+        CONFIGURE_COMMAND ${HOST_ENV_CMAKE} <SOURCE_DIR>/source/configure --enable-static --prefix=${CMAKE_CURRENT_BINARY_DIR}/icu_host
+        BUILD_COMMAND ${HOST_ENV_CMAKE} ${MAKE_PROGRAM} -j ${NUM_JOBS}
+        INSTALL_COMMAND ${HOST_ENV_CMAKE} ${MAKE_PROGRAM} install
+        COMMAND ${HOST_CMAKE_RELOAD}
+)
+add_dependencies(icu icu_host)
+
+if (ICU_CROSS_ARCH)
+    if (ANDROID)
+        # copy over both sysroots to a common sysroot (workaround ICU failing without one single sysroot)
+        file(COPY ${ANDROID_SYSTEM_LIBRARY_PATH}/usr DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/)
+        file(COPY ${CMAKE_SYSROOT}/usr/include DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/usr/)
+
+        # for c++ standard headers
+        set(CROSS_INCLUDES "")
+        foreach(INC ${CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES})
+            set(CROSS_INCLUDES "${CROSS_INCLUDES} -I${INC}")
+        endforeach()
+        
+        # for c++ standard libs
+        set(CROSS_LIBS ${CMAKE_C_STANDARD_LIBRARIES_INIT})
+        string(REPLACE "\" \"" ";" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
+        string(REPLACE "\"" "" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
+        foreach(LIB ${ANDROID_CXX_STANDARD_LIBRARIES})
+            get_filename_component(LIB_PATH ${LIB} DIRECTORY)
+            get_filename_component(LIB_NAME ${LIB} NAME_WE)
+            # remove starting "lib"
+            string(REGEX REPLACE "^lib" "" LIB_NAME ${LIB_NAME})
+            
+            set(CROSS_LIBS "${CROSS_LIBS} -L${LIB_PATH} -l${LIB_NAME}")
+        endforeach()
+
+        set(CROSS_CFLAGS "")
+        # fix http://bugs.icu-project.org/trac/ticket/12854
+        set(CROSS_CXXFLAGS "-DU_HAVE_XLOCALE_H=0")
+        set(CROSS_CC "${CMAKE_C_COMPILER} ${CMAKE_C_COMPILE_OPTIONS_EXTERNAL_TOOLCHAIN}${CMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN} --sysroot=${CMAKE_CURRENT_BINARY_DIR}/sysroot ${CMAKE_C_FLAGS} -target ${CMAKE_C_COMPILER_TARGET}")
+        set(CROSS_CXX "${CMAKE_CXX_COMPILER} ${CMAKE_CXX_COMPILE_OPTIONS_EXTERNAL_TOOLCHAIN}${CMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN} --sysroot=${CMAKE_CURRENT_BINARY_DIR}/sysroot ${CMAKE_CXX_FLAGS} ${CROSS_INCLUDES} -target ${CMAKE_CXX_COMPILER_TARGET}")
+        set(CROSS_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${CROSS_LIBS}")
+    else()
+        set(CROSS_CFLAGS "${CMAKE_C_FLAGS}")
+        set(CROSS_CXXFLAGS "${CMAKE_CXX_FLAGS}")
+        set(CROSS_CC "${CMAKE_C_COMPILER}")
+        set(CROSS_CXX "${CMAKE_CXX_COMPILER}")
+        set(CROSS_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
+    endif()
+
+    set(CROSS_ENV_CMAKE ${CMAKE_COMMAND} -E env
+            CC=${CROSS_CC}
+            CXX=${CROSS_CXX}
+            CFLAGS=${CROSS_CFLAGS}
+            CXXFLAGS=${CROSS_CXXFLAGS}
+            LDFLAGS=${CROSS_LDFLAGS}
+    )
+
+    ExternalProject_Add(
+            icu_cross
+            DEPENDS icu_host
+            SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu
+            BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_cross-build
+            PATCH_COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0020-workaround-missing-locale.patch || true
+            CONFIGURE_COMMAND ${CROSS_ENV_CMAKE} sh <SOURCE_DIR>/source/configure --enable-static --prefix=${CMAKE_CURRENT_BINARY_DIR}/icu_cross
+            --host=${ICU_CROSS_ARCH} --with-cross-build=${CMAKE_CURRENT_BINARY_DIR}/icu_host-build
+            BUILD_COMMAND ${CROSS_ENV_CMAKE} ${MAKE_PROGRAM} -j ${NUM_JOBS}
+            INSTALL_COMMAND ${CROSS_ENV_CMAKE} ${MAKE_PROGRAM} install
+            COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} ${CMAKE_BINARY_DIR}  # force CMake-reload
+    )
+    add_dependencies(icu icu_cross)
+
+    set(ICU_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_cross/ CACHE INTERNAL "" FORCE)
+else()
+    set(ICU_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_host/ CACHE INTERNAL "" FORCE)
+endif()
---
+++ cmake/PrebuiltICU.cmake
@@ -0,0 +1,55 @@
+# MIT License
+#
+# Copyright (c) 2018 The ViaDuck Project
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# check out prebuilts for the current system
+
+# includes
+include(ExternalProject)
+include(TargetArch)
+
+# autodetect PREBUILT_BRANCH
+target_architecture(ARCH)
+if (WIN32)
+    # prebuilts on windows use mingw-w64 for building
+    set(ARCH_SYSTEM ${ARCH}-w64-mingw32)
+elseif(UNIX AND NOT APPLE)
+    set(ARCH_SYSTEM ${ARCH}-linux)
+else()
+    message(FATAL_ERROR "Prebuilts this system are not available (yet)!")
+endif()
+message(STATUS "Using ${ARCH_SYSTEM} prebuilts")
+set(PREBUILT_BRANCH ${ARCH_SYSTEM} CACHE STRING "Branch in ICU-Prebuilts to checkout from")
+
+# add icu prebuilt target
+ExternalProject_Add(icu_pre
+        GIT_REPOSITORY https://gl.viaduck.org/viaduck/icu-prebuilts.git
+        GIT_TAG ${PREBUILT_BRANCH}
+
+        UPDATE_COMMAND ""
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        TEST_COMMAND ""
+)
+add_dependencies(icu icu_pre)
+set(ICU_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_pre-prefix/src/icu_pre/ CACHE INTERNAL "" FORCE)
