0db65409fac64166dad6e88a1ecbcecb99b68ee5
Alexandros Roussos <thealexroussos@gmail.com>
2018-05-07 16:56:56 +0200

    [ci skip] fixed prebuilts path

--- cmake/PrebuiltICU.cmake
+++ cmake/PrebuiltICU.cmake
@@ -28,9 +28,6 @@ include(ExternalProject)
 include(TargetArch)
 include(ByproductsICU)
 
-# predict icu variables
-GetICUByproducts(${CMAKE_CURRENT_BINARY_DIR}/icu_pre-prefix/src/icu_pre/${PREBUILT_BRANCH} ICU_LIBRARIES ICU_INCLUDE_DIRS)
-
 # autodetect PREBUILT_BRANCH
 target_architecture(ARCH)
 if (WIN32)
@@ -43,6 +40,9 @@ else()
 endif()
 message(STATUS "Using ${ARCH_SYSTEM} prebuilts")
 set(PREBUILT_BRANCH ${ARCH_SYSTEM} CACHE STRING "Branch in ICU-Prebuilts to checkout from")
+
+# predict icu variables
+GetICUByproducts(${CMAKE_CURRENT_BINARY_DIR}/icu_pre-prefix/src/icu_pre/${PREBUILT_BRANCH} ICU_LIBRARIES ICU_INCLUDE_DIRS)
 
 # add icu prebuilt target
 ExternalProject_Add(icu_pre
