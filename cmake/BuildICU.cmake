5d7b12510facb2c13c4323a3dc94efaebf6151bf
Alexandros Roussos <thealexroussos@gmail.com>
2019-07-08 19:39:30 +0200

    Fixed compilation on NDK r20 and android-29
    Removed stale patches for 63.1

--- cmake/BuildICU.cmake
+++ cmake/BuildICU.cmake
@@ -91,32 +91,38 @@ add_dependencies(icu icu_host)
 
 if (ICU_CROSS_ARCH)
     if (ANDROID)
+        set(CROSS_INCLUDES "")
+        set(CROSS_LIBS "")
+    
         # copy over both sysroots to a common sysroot (workaround ICU failing without one single sysroot)
-        file(COPY ${ANDROID_SYSTEM_LIBRARY_PATH}/usr DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/)
+        if (${ANDROID_NDK_MAJOR} GREATER 19)
+            string(REPLACE "-clang" "" ANDROID_TOOLCHAIN_NAME ${ANDROID_TOOLCHAIN_NAME})
+            file(COPY ${ANDROID_TOOLCHAIN_ROOT}/sysroot/usr/lib/${ANDROID_TOOLCHAIN_NAME}/${ANDROID_PLATFORM_LEVEL}/ DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/usr/lib/)
+            file(COPY ${ANDROID_TOOLCHAIN_ROOT}/sysroot/usr/lib/${ANDROID_TOOLCHAIN_NAME}/ DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/usr/lib/ PATTERN *.*)
+        else ()
+            file(COPY ${ANDROID_SYSTEM_LIBRARY_PATH}/usr DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/)
+            
+            # for c++ standard headers
+            foreach(INC ${CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES})
+                set(CROSS_INCLUDES "${CROSS_INCLUDES} -I${INC}")
+            endforeach()
+            
+            # for c++ standard libs
+            set(CROSS_LIBS ${CMAKE_C_STANDARD_LIBRARIES_INIT})
+            string(REPLACE "\" \"" ";" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
+            string(REPLACE "\"" "" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
+            foreach(LIB ${ANDROID_CXX_STANDARD_LIBRARIES})
+                get_filename_component(LIB_PATH ${LIB} DIRECTORY)
+                get_filename_component(LIB_NAME ${LIB} NAME_WE)
+                # remove starting "lib"
+                string(REGEX REPLACE "^lib" "" LIB_NAME ${LIB_NAME})
+                
+                set(CROSS_LIBS "${CROSS_LIBS} -L${LIB_PATH} -l${LIB_NAME}")
+            endforeach()
+        endif()
         file(COPY ${CMAKE_SYSROOT}/usr/include DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/sysroot/usr/)
 
-        # for c++ standard headers
-        set(CROSS_INCLUDES "")
-        foreach(INC ${CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES})
-            set(CROSS_INCLUDES "${CROSS_INCLUDES} -I${INC}")
-        endforeach()
-        
-        # for c++ standard libs
-        set(CROSS_LIBS ${CMAKE_C_STANDARD_LIBRARIES_INIT})
-        string(REPLACE "\" \"" ";" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
-        string(REPLACE "\"" "" ANDROID_CXX_STANDARD_LIBRARIES ${ANDROID_CXX_STANDARD_LIBRARIES})
-        foreach(LIB ${ANDROID_CXX_STANDARD_LIBRARIES})
-            get_filename_component(LIB_PATH ${LIB} DIRECTORY)
-            get_filename_component(LIB_NAME ${LIB} NAME_WE)
-            # remove starting "lib"
-            string(REGEX REPLACE "^lib" "" LIB_NAME ${LIB_NAME})
-            
-            set(CROSS_LIBS "${CROSS_LIBS} -L${LIB_PATH} -l${LIB_NAME}")
-        endforeach()
-
         set(CROSS_CFLAGS "")
-        # fix http://bugs.icu-project.org/trac/ticket/12854
-        set(CROSS_CXXFLAGS "-DU_HAVE_XLOCALE_H=0")
         set(CROSS_CC "${CMAKE_C_COMPILER} ${CMAKE_C_COMPILE_OPTIONS_EXTERNAL_TOOLCHAIN}${CMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN} --sysroot=${CMAKE_CURRENT_BINARY_DIR}/sysroot ${CMAKE_C_FLAGS} -target ${CMAKE_C_COMPILER_TARGET}")
         set(CROSS_CXX "${CMAKE_CXX_COMPILER} ${CMAKE_CXX_COMPILE_OPTIONS_EXTERNAL_TOOLCHAIN}${CMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN} --sysroot=${CMAKE_CURRENT_BINARY_DIR}/sysroot ${CMAKE_CXX_FLAGS} ${CROSS_INCLUDES} -target ${CMAKE_CXX_COMPILER_TARGET}")
         set(CROSS_LDFLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${CROSS_LIBS}")
@@ -144,8 +150,7 @@ if (ICU_CROSS_ARCH)
             DEPENDS icu_host
             SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu
             BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/icu_cross-build
-            PATCH_COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0020-workaround-missing-locale.patch || true
-            COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0022-fix-namespace.patch || true
+            PATCH_COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0022-fix-namespace.patch || true
             COMMAND ${PATCH_PROGRAM} -p1 --forward -r - < ${CMAKE_CURRENT_SOURCE_DIR}/patches/0023-remove-soname-version.patch || true
             CONFIGURE_COMMAND ${CROSS_ENV_CMAKE} sh <SOURCE_DIR>/source/configure --enable-static --prefix=${CMAKE_CURRENT_BINARY_DIR}/icu_cross
             --libdir=${CMAKE_CURRENT_BINARY_DIR}/icu_cross/lib/ --host=${ICU_CROSS_ARCH} --with-cross-build=${CMAKE_CURRENT_BINARY_DIR}/icu_host-build
